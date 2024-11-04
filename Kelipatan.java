0b947dc3ce65f1fc69f9c52abbabcb05fe717adc
FurkotunNajiyah <onlyuqoh@gmail.com>
2024-11-04 18:00:31 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * 
+ *
+ * @author ACER
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        double bilangan = 3.5;
+        double batas = 35.0;
+
+        while (bilangan <= batas) {
+            System.out.println(bilangan);
+            bilangan += 3.5;
+        }
+    }
+}
